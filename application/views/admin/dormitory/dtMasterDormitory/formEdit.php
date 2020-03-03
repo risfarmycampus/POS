97c9b9d3a7c192933401f5f12e0a399ee51adab6
Fiqi Hardiyansah <fiqi316@gmail.com>
2020-03-04 04:27:02 +0700

    commit master

---
+++ application/views/admin/dormitory/dtMasterDormitory/formEdit.php
@@ -0,0 +1,380 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Form Edit
+        <small></small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="#">Data Dormitory</a></li>
+        <li class="active">Form Edit</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <!-- left column -->
+        <!--/.col (left) -->
+        <!-- right column -->
+        <div class="col-md-12">
+          <!-- Horizontal Form -->
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Data Dormitory</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+			<?php foreach ($tbl_dormitory  as $data): ?>
+            <form action="<?php echo base_url('admin/upadteDormitory');?>" method="post" class="form-horizontal">
+              <div class="box-body">
+								<div class="form-group">
+                  <label for="inputPassword3" class="col-sm-3 control-label">Floor</label>
+
+                  <div class="col-sm-5">
+                    <select name="floor" required class="form-control">
+					<option value="<?php echo $data->floor ?>"><?php echo $data->floor ?></option>
+                    <option value=""> -- Select floor -- </option>
+                    <option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+                  </select>
+                  </div>
+                </div>
+				<div class="form-group">
+                  <label for="inputEmail3" class="col-sm-3 control-label">Type</label>
+
+                  <div class="col-sm-5">
+                    <select name="type" required class="form-control">
+					<option value="<?php echo $data->type ?>"><?php echo $data->type ?></option>
+                    <option value=""> -- Select type -- </option>
+                    <option value="Suite">Suite</option>
+					<option value="Family">Family</option>
+					<option value="Deluxe A">Deluxe A</option>
+					<option value="Deluxe B">Deluxe B</option>
+					<option value="Superior A">Superior A</option>
+					<option value="Superior B">Superior B</option>
+					<option value="Superior C">Superior C</option>
+					<option value="Standard A">Standard A</option>
+					<option value="Standard B">Standard B</option>
+					<option value="Standard C">Standard C</option>
+                  </select>
+                  </div>
+                </div>
+				<div class="form-group">
+                  <label for="inputEmail3" required class="col-sm-3 control-label">Bed</label>
+
+                  <div class="col-sm-5">
+                  <div class="radio">
+				  <?php 
+					$bed1 = "";
+					$bed2 = "";
+					$bed3 = "";
+					$bed4 = "";
+						if($data->bed == "1"){
+							$bed1 = "checked";
+						} else if($data->bed == "2"){
+							$bed2 = "checked";
+						} else if($data->bed == "3"){
+							$bed3 = "checked";
+						} else if($data->bed == "4") {
+							$bed4 = "checked";
+						}
+					?>
+                    <label>
+                      <input type="radio" required name="bed" <?= $bed1; ?> required id="optionsRadios1" value="1">
+                      1
+                    </label>
+					<label>
+                      <input type="radio" required name="bed" <?= $bed2; ?> required id="optionsRadios1" value="2">
+                      2
+                    </label>
+					<label>
+                      <input type="radio" required name="bed" <?= $bed3; ?> required id="optionsRadios1" value="3">
+                      3
+                    </label>
+					<label>
+                      <input type="radio" required name="bed" <?= $bed4; ?> required id="optionsRadios1" value="4">
+                      4
+                    </label>
+                  </div>
+                  </div>
+                </div>
+				<div class="form-group">
+                  <label for="inputEmail3" class="col-sm-3 control-label">Room Size</label>
+
+                  <div class="col-sm-5">
+                    <input type="text" required name="room_size" value="<?php echo $data->room_size ?>" class="form-control" id="inputEmail3" placeholder="Input room size ...">
+                    <input type="hidden" name="id" value="<?php echo $data->id ?>" class="form-control" id="inputEmail3" placeholder="Input room size ...">
+                  </div>
+                </div>
+				<div class="form-group">
+                  <label for="inputEmail3" class="col-sm-3 control-label">Room Quantity</label>
+
+                  <div class="col-sm-5">
+                    <div class="row">
+					<div class="col-xs-6">
+					  <input type="text" name="qty_male" required value="<?php echo $data->qty_male ?>" class="form-control" placeholder="Male ...">
+					</div>
+					<div class="col-xs-6">
+					  <input type="text" name="qty_female" required value="<?php echo $data->qty_female ?>" class="form-control" placeholder="Female ...">
+					</div>
+				  </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword3" class="col-sm-3 control-label">Facilities</label>
+
+                  <div class="col-sm-5">
+                    <textarea name="facilities" required class="form-control" rows="3" placeholder="Input facilities ..."><?php echo $data->facilities ?></textarea>
+                  </div>
+                </div>
+				<div class="form-group">
+                  <label for="inputEmail3" class="col-sm-3 control-label">Price</label>
+
+                  <div class="col-sm-5">
+                    <input type="text" name="price" required value="<?php echo $data->price ?>" class="form-control" id="inputEmail3" placeholder="Input price ...">
+                  </div>
+                </div>
+              </div>
+              <!-- /.box-body -->
+              <div class="box-footer">
+			  <div class="col-sm-offset-3 col-sm-10">
+                <a href="<?php echo base_url('admin/dtDormitory');?> "type="submit" class="btn btn-default">Batal</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <button type="submit" class="btn btn-info">Simpan</button>
+			  </div>
+              </div>
+              <!-- /.box-footer -->
+            </form>
+			<?php endforeach ?>
+          </div>
+          <!-- /.box -->
+          <!-- general form elements disabled -->
+          <!-- /.box -->
+        </div>
+        <!--/.col (right) -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.4.13
+    </div>
+    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
+    reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Create the tabs -->
+    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+      <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
+      <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+    </ul>
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <!-- Home tab content -->
+      <div class="tab-pane" id="control-sidebar-home-tab">
+        <h3 class="control-sidebar-heading">Recent Activity</h3>
+        <ul class="control-sidebar-menu">
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-birthday-cake bg-red"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
+
+                <p>Will be 23 on April 24th</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-user bg-yellow"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
+
+                <p>New phone +1(800)555-1234</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
+
+                <p>nora@example.com</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-file-code-o bg-green"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
+
+                <p>Execution time 5 seconds</p>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <!-- /.control-sidebar-menu -->
+
+        <h3 class="control-sidebar-heading">Tasks Progress</h3>
+        <ul class="control-sidebar-menu">
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Custom Template Design
+                <span class="label label-danger pull-right">70%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Update Resume
+                <span class="label label-success pull-right">95%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-success" style="width: 95%"></div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Laravel Integration
+                <span class="label label-warning pull-right">50%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Back End Framework
+                <span class="label label-primary pull-right">68%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <!-- /.control-sidebar-menu -->
+
+      </div>
+      <!-- /.tab-pane -->
+      <!-- Stats tab content -->
+      <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
+      <!-- /.tab-pane -->
+      <!-- Settings tab content -->
+      <div class="tab-pane" id="control-sidebar-settings-tab">
+        <form method="post">
+          <h3 class="control-sidebar-heading">General Settings</h3>
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Report panel usage
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+
+            <p>
+              Some information about this general settings option
+            </p>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Allow mail redirect
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+
+            <p>
+              Other sets of options are available
+            </p>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Expose author name in posts
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+
+            <p>
+              Allow the user to show his name in blog posts
+            </p>
+          </div>
+          <!-- /.form-group -->
+
+          <h3 class="control-sidebar-heading">Chat Settings</h3>
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Show me as online
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Turn off notifications
+              <input type="checkbox" class="pull-right">
+            </label>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Delete chat history
+              <a href="javascript:void(0)" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
+            </label>
+          </div>
+          <!-- /.form-group -->
+        </form>
+      </div>
+      <!-- /.tab-pane -->
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="<?php echo base_url();?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url();?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="<?php echo base_url();?>assets/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url();?>assets/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url();?>assets/dist/js/demo.js"></script>
+</body>
+</html>
